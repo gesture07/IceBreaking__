3a97b33fd1a9a5620cf9ff75fb92fdf5de751fd2
JHR <115974764+haeri-J@users.noreply.github.com>
2023-07-26 19:45:13 +0900

    create room

---
+++ Assets/Scripts/h_GameManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class h_GameManager : MonoBehaviourPunCallbacks
+{
+
+    #region Photon Callbacks
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    #endregion
+}
+     
+  
