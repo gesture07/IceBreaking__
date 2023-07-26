45fe5694f9a95cf96d38fc0c2c94a77a85d310a4
JHR <115974764+haeri-J@users.noreply.github.com>
2023-07-26 20:25:43 +0900

    방 10명 버전

--- Assets/Scripts/h_GameManager.cs
+++ Assets/Scripts/h_GameManager.cs
@@ -9,6 +9,20 @@ using Photon.Realtime;
 
 public class h_GameManager : MonoBehaviourPunCallbacks
 {
+    #region Private Methods
+
+    void LoadArena()
+    {
+        if(!PhotonNetwork.IsMasterClient)//마스터 클라이언트인지 체크크
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            return;
+        }
+        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);//"PhotonNetwork.CurrentRoom.PlayerCount 속성을 사용하여 플레이어 수를 가져
+        PhotonNetwork.LoadLevel("Room for "+ PhotonNetwork.CurrentRoom.PlayerCount); //PhotonNetwork.LoadLevel()을 사용하여 새로운 레벨 또는 씬을 로드합니다
+    }
+
+    #endregion
 
     #region Photon Callbacks
 
